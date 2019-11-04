d0ea215560f712dd81eb398bfa982b8f877eb8c0
chenbin <chenbin@higgsdynamics.com>
2019-11-04 17:47:11 +0800

    上传后端名字以及sunny-ngrok工具

--- appChat/admin.py
+++ appChat/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import UserInfo
+
+admin.site.site_header = "叨叨管理"
+
 class UserIntoTable(admin.ModelAdmin):
     list_display = ('userid', 'password', 'nickname', 'sex', 'birthday',
                     'personalsign', 'landtime', 'registertime', )
---
+++ windows_amd64/start.bat
@@ -0,0 +1,6 @@
+:TUNNEL
+set clientid=f650c8892a08c32f
+echo.
+sunny.exe clientid %clientid%
+PAUSE
+goto TUNNEL
