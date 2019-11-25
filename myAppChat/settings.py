8835bd7e3fe93578ac283cce3add28495f802987
chenbin <824936026@qq.com>
2019-11-25 12:36:43 +0800

    上传django部署完成的结构

--- myAppChat/settings.py
+++ myAppChat/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -44,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+#    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -121,7 +120,6 @@ USE_L10N = True
 
 #USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
@@ -130,17 +128,22 @@ STATIC_URL = '/static/'
 # 当运行 python manage.py collectstatic 的时候
 # STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
 # 把这些文件放到一起是为了用apache等部署的时候更方便
-STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
 # 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "common_static"),
-    ('css', os.path.join(STATIC_ROOT, 'css').replace('\\', '/')),
-    ('images', os.path.join(STATIC_ROOT, 'images').replace('\\', '/')),
-    ('fonts', os.path.join(STATIC_ROOT, 'fonts').replace('\\', '/')),
-    ('js', os.path.join(STATIC_ROOT, 'js').replace('\\', '/')),
-)
+
+STATICFILES_DIRS = [
+    os.path.join(STATIC_ROOT, 'admin/css'),
+    os.path.join(STATIC_ROOT, 'admin/img'),
+    os.path.join(STATIC_ROOT, 'admin/fonts'),
+    os.path.join(STATIC_ROOT, 'admin/js'),
+    ('css', os.path.join(STATIC_ROOT, 'css')),
+    ('img', os.path.join(STATIC_ROOT, 'img')),
+    ('fonts', os.path.join(STATIC_ROOT, 'fonts')),
+    ('js', os.path.join(STATIC_ROOT, 'js')),
+]
 
 # 这个是默认设置，Django 默认会在 STATICFILES_DIRS中的文件夹 和 各app下的static文件夹中找文件
 # 注意有先后顺序，找到了就不再继续找了
@@ -148,3 +151,4 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
 )
+
